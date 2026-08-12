ae529f73cb36a140f16cc0aab18e67acb3c4f79b
Gustavo Gomes <gustavocjr.gg@gmail.com>
2026-08-12 18:49:15 -0300

    update requirements.txt

--- livros/admin.py
+++ livros/admin.py
@@ -1,22 +1,29 @@
 from django.contrib import admin
 from .models import Livro, Emprestimo, MensagemSuporte
 
-# Register your models here.
-
-from django.contrib import admin
-from .models import Livro
-
 @admin.register(Livro)
 class LivroAdmin(admin.ModelAdmin):
     list_display = ('id', 'titulo', 'autor', 'editora', 'ano_publicacao', 'quantidade_exemplares', 'disponivel')
     search_fields = ('titulo', 'autor')
     list_filter = ('disponivel', 'ano_publicacao')
 
+@admin.action(description='Marcar como devolvido')
+def marcar_devolvido(modeladmin, request, queryset):
+    for emprestimo in queryset.filter(devolvido=False):
+        emprestimo.devolvido = True
+        emprestimo.save()
+
+        livro = emprestimo.livro
+        livro.quantidade_exemplares += 1
+        livro.disponivel = True
+        livro.save()
+
 @admin.register(Emprestimo)
 class EmprestimoAdmin(admin.ModelAdmin):
     list_display = ('id', 'livro', 'nome_completo', 'cpf', 'data_emprestimo', 'devolvido')
     search_fields = ('nome_completo', 'cpf')
     list_filter = ('devolvido',)
+    actions = [marcar_devolvido]
 
 @admin.register(MensagemSuporte)
 class MensagemSuporteAdmin(admin.ModelAdmin):
